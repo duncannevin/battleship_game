27b8e127ca8829bbfee96e522aac90af8d8110b1
duncan nevin <duncan.nevin@gmail.com>
2021-02-19 19:30:04 -0800

    cleans

--- models/play.js
+++ models/play.js
@@ -1,6 +1,6 @@
 const {DataTypes, Model} = require('sequelize');
-const log = require('log4js').getLogger('db:user');
 const { v4: uuid } = require('uuid');
+const { getBoardById } = require('../services/board');
 
 class Play extends Model {}
 
@@ -39,6 +39,72 @@ function initialize(db) {
     });
 
     Play.beforeCreate(play => play.id = uuid());
+    Play.beforeCreate(play => {
+        play.hit = detectHit(play);
+    });
 }
 
 module.exports = { Play, initialize };
+
+async function detectHit({ BoardId, x, y }) {
+    const { Ships } = await getBoardById(BoardId);
+    return findHit(Ships, x, y);
+
+    function findHit(ships, x, y) {
+        if (!ships.length) {
+            return false;
+        }
+
+        const current = ships.shift();
+
+        if (current.orientation === 'n') {
+            // size = 3
+            /// x = 0/0 bow (x, y)
+            /// x = 1/0
+            /// x = 2/0 stern
+            for (let i = current.x; i <= (current.x + current.size); i++) {
+                if (i === x && current.y === y) {
+                    return true;
+                }
+            }
+        }
+
+        if (current.orientation === 'e') {
+            // size = 3
+            /// x = 0/2 stern
+            /// x = 0/1
+            /// x = 0/0 bow (x,y)
+            for (let i = current.y; i >= (current.y + current.size); i++) {
+                if (i === y && current.x === x) {
+                    return true;
+                }
+            }
+        }
+
+        if (current.orientation === 's') {
+            // size = 3
+            /// x = 2/0 stern
+            /// x = 1/0
+            /// x = 0/0 bow (x,y)
+            for (let i = current.x; i >= (current.x - current.size); i--) {
+                if (i === x && current.y === y ) {
+                    return true;
+                }
+            }
+        }
+
+        if (current.orientation === 'w') {
+            // size = 3
+            /// x = 0/0 bow (x,y)
+            /// x = 0/1
+            /// x = 0/2 stern
+            for (let i = current.y; y <= (current.y + current.size); i++) {
+                if (i === y && current.x === x) {
+                    return true;
+                }
+            }
+        }
+
+        return findHit(ships, x, y);
+    }
+}
